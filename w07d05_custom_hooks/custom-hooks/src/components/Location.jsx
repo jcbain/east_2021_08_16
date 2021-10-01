ffd196d3f158f0050a8840e88644ae744a38b2f9
James Bain <james.bain@lighthouselabs.com>
2021-10-01 11:13:12 -0600

    clean up lecture code

--- w07d05_custom_hooks/custom-hooks/src/components/Location.jsx
+++ w07d05_custom_hooks/custom-hooks/src/components/Location.jsx
@@ -1,45 +1,7 @@
-import {useEffect, useState} from 'react';
 import useLocation from '../hooks/useLocation';
 
 const Location = () => {
   const coords = useLocation();
-  // const [coords, setCoords] = useState({
-  //   lat: 0,
-  //   lng: 0, 
-  //   error: null, 
-  //   resolved: false
-  // })
-
-  // useEffect(() => {
-  //   const handleSuccess = (data) => {
-  //     const latNoise = Math.random();
-  //     const lngNoise = Math.random();
-
-  //     const lat = data.coords.latitude + latNoise;
-  //     const lng = data.coords.longitude + lngNoise;
-
-  //     console.log(lat, lng)
-
-  //     setCoords({
-  //       lat: lat,
-  //       lng: lng,
-  //       error: null,
-  //       resolved: true
-  //     })
-  //   }
-
-  //   const handleError = (err) => {
-  //     console.log(err.message)
-
-  //     setCoords( prev => ({
-  //       ...prev,
-  //       error: err.message,
-  //       resolved: true
-  //     }))
-  //   }
-
-  //   navigator.geolocation.getCurrentPosition(handleSuccess, handleError)
-  // }, [])
 
   return (
     <div>
